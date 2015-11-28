d1770397483c71adc86988158b8a32d78cba55c9
jksniper <>
2015-11-28 18:04:12 +0800

    Initial Commit

---
+++ ConanToolKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConanToolKit
+//
+//  Created by jksniper on 15/11/28.
+//  Copyright © 2015年 jksniper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
